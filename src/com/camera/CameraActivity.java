1cf69b5943d8b0f6f50580c0d5ace4b8bfe10597
Osaide Ogbeifun <osaide@gmail.com>
2013-02-05 15:31:09 -0800

    removed key

--- src/com/camera/CameraActivity.java
+++ src/com/camera/CameraActivity.java
@@ -14,11 +14,13 @@ import com.parse.ParseObject;
 public class CameraActivity extends Activity implements  Camera.PictureCallback{
 	private Button shutterButton;
 	private CameraPreview cameraPreview;
+    private String key = "key";
+    private String id = "id";
 
 	@Override
 	public  void onCreate(Bundle b){
 		super.onCreate(b);
-        Parse.initialize(this, "38faGNV3UuHWiepxRkL3u0ofx80BXVFGSF2S2E2v", "ETLJ7Lcujc9m2Eq0CE9cb8ldDqEwgu2vTaKdHnCs");
+        Parse.initialize(this, key, id);
         setContentView(R.layout.camera);
 		cameraPreview = new CameraPreview(this);
 		RelativeLayout preview = (RelativeLayout) findViewById(R.id.camera);
